03f4cfb873282ffa219ccf11fc333fb555b742e8
vbox <saagrsaaml@gmail.com>
2018-06-27 14:24:41 +0545

    add token in header

--- src/axios.auth.js
+++ src/axios.auth.js
@@ -1,6 +1,16 @@
 import axios from 'axios'
 import { app } from './app.config'
 
+const getHeader = () => {
+  const tokenData = JSON.parse(window.localStorage.getItem('token'))
+  const headers = {
+    'Accept': 'application/json',
+    'Authorization': 'Token ' + tokenData
+  }
+  return headers
+}
+
 export const $http = axios.create({
-  baseURL: app.APIENDPOINT
+  baseURL: app.APIENDPOINT,
+  ...(window.localStorage.getItem('token') ? { headers: getHeader() } : {})
 })
