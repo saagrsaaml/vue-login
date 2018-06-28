07b34921dc2daeba232616dd428d8ca0d63aace6
vbox <saagrsaaml@gmail.com>
2018-06-28 16:13:19 +0545

    show auth user details in profile page

--- src/components/user/profile/home/home.vue
+++ src/components/user/profile/home/home.vue
@@ -1,6 +1,12 @@
 <script>
+import { mapGetters } from 'vuex'
 export default {
   template: require('./home.html'),
-  name: 'user'
+  name: 'user',
+  computed: {
+    ...mapGetters([
+      'authUser'
+    ])
+  }
 }
 </script>
